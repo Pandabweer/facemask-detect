891d47ce68e13e54a374317dd9c4d6cd2780e89a
Enrique bos <54767698+Pandabweer@users.noreply.github.com>
2021-11-04 09:21:33 +0100

    Update detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -90,8 +90,6 @@ class Train_mask_detector(object):
 		headModel = Dropout(0.5)(headModel)
 		headModel = Dense(2, activation="softmax")(headModel)
 
-		# place the head FC model on top of the base model (this will become
-		# the actual model we will train)
 		model = Model(inputs=baseModel.input, outputs=headModel)
 
 		for layer in baseModel.layers:
@@ -160,8 +158,6 @@ class Main(object):
 		return VideoStream(src=0).start()
 
 	async def detect_and_predict_mask(self, frame: np.ndarray, face_net: cv2.dnn_Net, mask_net: Functional) -> typing.Union[tuple, None]:
-		# grab the dimensions of the frame and then construct a blob from it
-		# then pass the blob through the network and obtain the face detections
 		h, w = frame.shape[:2]
 		blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
 
@@ -174,15 +170,11 @@ class Main(object):
 			# extract the confidence (i.e., probability) associated with the detection
 			confidence = detections[0, 0, i, 2]
 
-			# filter out weak detections by ensuring the confidence is
-			# greater than the minimum confidence
 			if confidence > 0.5:
 				# compute the (x, y)-coordinates of the bounding box for the object
 				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 				startX, startY, endX, endY = box.astype('int')
 
-				# ensure the bounding boxes fall within the dimensions of the frame
-				# extract the face ROI, convert it from BGR to RGB channel
 				# ordering, resize it to 224x224, and preprocess it
 				face = preprocess_input(img_to_array(cv2.resize(cv2.cvtColor(
 					frame[
@@ -198,9 +190,6 @@ class Main(object):
 
 		# only make a predictions if at least one face was detected
 		if len(faces) > 0:
-			# for faster inference we'll make batch predictions on *all*
-			# faces at the same time rather than one-by-one predictions
-			# in the above `for` loop
 			faces = np.array(faces, dtype='float32')
 			preds = mask_net.predict(faces, batch_size=32)
 
@@ -209,10 +198,8 @@ class Main(object):
 
 	async def detect_mask(self) -> None:
 		print('[INFO] Programma is gestart!')
-		# loop over the frames from the video stream
 		while True:
-			# grab the frame from the threaded video stream and resize it
-			# to have a maximum width of 1920 pixels
+			# video of 1920 pixels
 			frame = imutils.resize(self.vs.read(), width=1920)
 
 			# detect faces in the frame and determine if they are wearing a face mask or not
@@ -220,18 +207,16 @@ class Main(object):
 
 			# loop over the detected face locations and their corresponding locations
 			for box, pred in zip(locs, preds):
-				# unpack the bounding box and predictions
+				# unpack the box and predictions
 				startX, startY, endX, endY = box
 				mask, withoutMask = pred
 
-				# determine the class label and color we'll use to draw the bounding box and text
 				label = ('Masker' if mask > withoutMask else 'Geen masker')
 				color = (0, 255, 0) if label == 'Masker' else (0, 0, 255)
 
 				# include the probability in the label
 				label = '{}: {:.2f}%'.format(label, max(mask, withoutMask) * 100)
 
-				# display the label and bounding box rectangle on the output frame
 				cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 				cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
@@ -241,11 +226,9 @@ class Main(object):
 			# tick (fps) -> 60/(0.04*60) = 25 fps
 			#time.sleep(0.03)
 
-			# if the `q` key was pressed, break from the loop
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				print('[INFO] Programma beëindigd')
 
-				# do a bit of cleanup
 				cv2.destroyAllWindows()
 				self.vs.stop()
 				break
